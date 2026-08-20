95849c4d699dd1e35b3999de182f8dd729afe89d
adourix <137846847+adourix@users.noreply.github.com>
2026-08-20 19:23:02 +0300

    Enhance database connection with logging
    Added detailed logging for database path and existence check.

--- lib/core/database/connection.dart
+++ lib/core/database/connection.dart
@@ -8,10 +8,51 @@ import 'package:path_provider/path_provider.dart';
 QueryExecutor openConnection() {
   return driftDatabase(
     name: 'lingo_store.db',
+
     native: DriftNativeOptions(
       databaseDirectory: () async {
+
         final dir = await getApplicationDocumentsDirectory();
-        return Directory(p.join(dir.path, 'database'));
+
+        final databaseDir = Directory(
+          p.join(
+            dir.path,
+            'database',
+          ),
+        );
+
+        final databaseFile = File(
+          p.join(
+            databaseDir.path,
+            'lingo_store.db',
+          ),
+        );
+
+
+        print("=================================");
+        print("LINGO STORE DATABASE PATH");
+        print("Documents:");
+        print(dir.path);
+
+        print("");
+
+        print("Database folder:");
+        print(databaseDir.path);
+
+        print("");
+
+        print("Database file:");
+        print(databaseFile.path);
+
+        print("");
+
+        print("Database exists:");
+        print(await databaseFile.exists());
+
+        print("=================================");
+
+
+        return databaseDir;
       },
     ),
   );
