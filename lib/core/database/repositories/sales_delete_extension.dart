8cf457dfd3f9f16e2e27f431064ab0862d29ab12
adourix <137846847+adourix@users.noreply.github.com>
2026-09-01 08:17:13 +0300

    Fix invoice delete flow and restore customer balance

--- lib/core/database/repositories/sales_delete_extension.dart
+++ lib/core/database/repositories/sales_delete_extension.dart
@@ -5,6 +5,10 @@ import 'package:drift/drift.dart';
 extension SalesDeleteExtension on SalesRepository {
   Future<void> deleteSaleCompletely(int saleId) async {
     await db.transaction(() async {
+      final sale = await (db.select(db.sales)
+            ..where((tbl) => tbl.id.equals(saleId)))
+          .getSingle();
+
       final items = await (db.select(db.saleItems)
             ..where((tbl) => tbl.saleId.equals(saleId)))
           .get();
@@ -21,6 +25,21 @@ extension SalesDeleteExtension on SalesRepository {
             .write(
           ProductsCompanion(
             quantity: Value(product.quantity + item.quantity),
+            updatedAt: Value(DateTime.now()),
+          ),
+        );
+      }
+
+      if (sale.customerId != null && sale.remaining > 0) {
+        final customer = await (db.select(db.customers)
+              ..where((tbl) => tbl.id.equals(sale.customerId!)))
+            .getSingle();
+
+        await (db.update(db.customers)
+              ..where((tbl) => tbl.id.equals(customer.id)))
+            .write(
+          CustomersCompanion(
+            balance: Value(customer.balance - sale.remaining),
             updatedAt: Value(DateTime.now()),
           ),
         );
