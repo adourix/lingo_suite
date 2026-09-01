6736c9dc9b7b852daa767f0db23fe4e48bb7399c
adourix <137846847+adourix@users.noreply.github.com>
2026-09-01 08:50:58 +0300

    revert: keep invoices provider compatible until repository stream is added

--- lib/core/providers/invoices_provider.dart
+++ lib/core/providers/invoices_provider.dart
@@ -3,8 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../database/app_database.dart';
 import 'sales_repository_provider.dart';
 
-final invoicesProvider = StreamProvider<List<Sale>>((ref) {
+final invoicesProvider = FutureProvider<List<Sale>>((ref) async {
   final repo = ref.watch(salesRepositoryProvider);
 
-  return repo.watchAllSales();
+  return repo.getAllSales();
 });
